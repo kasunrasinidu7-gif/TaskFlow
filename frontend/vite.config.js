affa00804834bfedcd725ff645b55d5206c3b2e7
kasunrasinidu7-gif <kasunrasinidu7@gmail.com>
2026-06-20 20:38:52 +0530

    changes in sever due to error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -23,9 +22,4 @@ export default defineConfig({
       },
     },
   },
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 686cf58 (frontend initial setup)
 })
